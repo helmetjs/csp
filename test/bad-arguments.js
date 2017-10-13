3e2a8d8740df4ea7879c99f4a0d5a88086e18cbd
Evan Hahn <me@evanhahn.com>
2017-10-13 10:44:26 -0400

    Improve testing for bad arguments; remove test dependency on source file

--- test/bad-arguments.js
+++ test/bad-arguments.js
@@ -4,197 +4,248 @@ var config = require('../lib/config')
 var camelize = require('camelize')
 var assert = require('assert')
 
+var SOURCELIST_DIRECTIVES = [
+  'base-uri', 'child-src', 'connect-src', 'default-src', 'font-src',
+  'form-action', 'frame-ancestors', 'frame-src', 'img-src',
+  'manifest-src', 'media-src', 'object-src', 'script-src', 'style-src',
+  'worker-src'
+]
+var SOURCELISTS_WITH_UNSAFES = ['script-src', 'style-src', 'worker-src']
+var BOOLEAN_DIRECTIVES = ['block-all-mixed-content', 'upgrade-insecure-requests']
+var PLUGINTYPE_DIRECTIVES = ['plugin-types']
+var URI_DIRECTIVES = ['report-to', 'report-uri']
+var SANDBOX_DIRECTIVES = ['sandbox']
+
 describe('with bad arguments', function () {
-  function throwTest (args, message) {
-    var err
-
-    try {
-      csp.apply(null, args)
-    } catch (e) {
-      err = e
-    }
-
-    assert(err instanceof Error, 'Calling with ' + args + ' did not throw an error')
-    assert.equal(err.message, message)
-  }
-
   describe('missing directives', function () {
     it('errors without arguments', function () {
-      throwTest([], 'csp must be called with arguments. See the documentation.')
+      assertThrowsWithArg('NO_ARGUMENTS', 'csp must be called with an object argument. See the documentation.')
+    })
+
+    it('errors with non-objects', function () {
+      [
+        undefined,
+        null,
+        true,
+        false,
+        0,
+        1,
+        '',
+        'str',
+        [],
+        [{ directives: {} }]
+      ].forEach(function (arg) {
+        assertThrowsWithArg(arg, 'csp must be called with an object argument. See the documentation.')
+      })
     })
 
     it('errors with an object that has no "directives" key', function () {
-      throwTest([{}], 'csp must have at least one directive under the "directives" key. See the documentation.')
-      throwTest([{ reportOnly: true }], 'csp must have at least one directive under the "directives" key. See the documentation.')
+      assertThrowsWithArg({}, 'csp must have at least one directive under the "directives" key. See the documentation.')
+      assertThrowsWithArg({ reportOnly: true }, 'csp must have at least one directive under the "directives" key. See the documentation.')
     })
 
     it('errors with an empty directives object', function () {
-      throwTest([{ directives: {} }], 'csp must have at least one directive under the "directives" key. See the documentation.')
+      assertThrowsWithArg({ directives: {} }, 'csp must have at least one directive under the "directives" key. See the documentation.')
     })
 
     it('errors with an invalid directive type', function () {
-      throwTest([{
+      assertThrowsWithArg({
         directives: { 'fart-man': ['http://example.com'] }
-      }], '"fart-man" is an invalid directive. See the documentation for the supported list. Force this by enabling loose mode.')
+      }, '"fart-man" is an invalid directive. See the documentation for the supported list. Force this by enabling loose mode.')
     })
   })
 
-  Object.keys(config.directives).forEach(function (directiveKey) {
-    var directiveInfo = config.directives[directiveKey]
-    var camelizedKey = camelize(directiveKey)
+  it('tests all directives', function () {
+    var actualDirectives = Object.keys(config.directives)
+    var expectedDirectives = SOURCELIST_DIRECTIVES.concat(BOOLEAN_DIRECTIVES, PLUGINTYPE_DIRECTIVES, URI_DIRECTIVES, SANDBOX_DIRECTIVES)
+    assert.deepEqual(actualDirectives.sort(), expectedDirectives.sort())
+  })
 
-    ;[directiveKey, camelizedKey].forEach(function (key) {
-      function set (value) {
-        var directives = {}
-        directives[key] = value
-        return [{ directives: directives }]
-      }
+  SOURCELIST_DIRECTIVES.forEach(function (directive) {
+    [directive, camelize(directive)].forEach(function (key) {
+      describe(key + ' directive, a source list', function () {
+        it('errors with an empty array', function () {
+          assertThrowsWithDirective(key, [], directive + ' must have at least one value. To block everything, set ' + directive + ' to ["\'none\'"].')
+        })
 
-      describe(key + ' directive', function () {
-        if (directiveInfo.type === 'sourceList') {
-          it('errors with an empty array', function () {
-            throwTest(set([]), directiveKey + ' must have at least one value. To block everything, set ' + directiveKey + ' to ["\'none\'"].')
+        it('errors with a non-array', function () {
+          [
+            null,
+            undefined,
+            true,
+            {},
+            '',
+            'https://example.com',
+            function () {}
+          ].forEach(function (value) {
+            assertThrowsWithDirective(key, value, '"' + value + '" is not a valid value for ' + directive + '. Use an array of strings.')
           })
+        })
 
-          it('errors when called with an array that contains non-strings', function () {
-            throwTest(set(['http://example.com', 69, 'https://example.com']), '"69" is not a valid source expression. Only non-empty strings are allowed.')
-          })
+        it('errors with an array that contains non-numbers', function () {
+          assertThrowsWithDirective(key, ['http://example.com', 69, 'https://example.com'], '"69" is not a valid source expression. Only non-empty strings are allowed.')
+        })
 
-          it('errors when called with non-array values', function () {
-            [
-              null,
-              undefined,
-              true,
-              {},
-              '',
-              function () {}
-            ].forEach(function (value) {
-              throwTest(set(value), '"' + value + '" is not a valid value for ' + directiveKey + '. Use an array of strings.')
-            })
-          })
-
-          it('errors with unquoted "self"', function () {
-            throwTest(set(['self']), '"self" must be quoted in ' + directiveKey + '. Change it to "\'self\'" in your source list. Force this by enabling loose mode.')
-          })
-
-          it('errors with unquoted "none"', function () {
-            throwTest(set(['none']), '"none" must be quoted in ' + directiveKey + '. Change it to "\'none\'" in your source list. Force this by enabling loose mode.')
-          })
-
-          if (directiveInfo.hasUnsafes) {
-            it('errors when called with unquoted "unsafe-inline" or "unsafe-eval"', function () {
-              throwTest(set(['unsafe-inline']), '"unsafe-inline" must be quoted in ' + directiveKey + '. Change it to "\'unsafe-inline\'" in your source list. Force this by enabling loose mode.')
-              throwTest(set(['unsafe-eval']), '"unsafe-eval" must be quoted in ' + directiveKey + '. Change it to "\'unsafe-eval\'" in your source list. Force this by enabling loose mode.')
-            })
-          } else {
-            it('errors when called with unsafe-inline or unsafe-eval', function () {
-              throwTest(set(['unsafe-inline']), '"unsafe-inline" does not make sense in ' + directiveKey + '. Remove it.')
-              throwTest(set(['unsafe-eval']), '"unsafe-eval" does not make sense in ' + directiveKey + '. Remove it.')
-              throwTest(set(["'unsafe-inline'"]), '"\'unsafe-inline\'" does not make sense in ' + directiveKey + '. Remove it.')
-              throwTest(set(["'unsafe-eval'"]), '"\'unsafe-eval\'" does not make sense in ' + directiveKey + '. Remove it.')
-            })
+        it('errors with unquoted values', function () {
+          assertThrowsWithDirective(key, ['self'], '"self" must be quoted in ' + directive + '. Change it to "\'self\'" in your source list. Force this by enabling loose mode.')
+          assertThrowsWithDirective(key, ['none'], '"none" must be quoted in ' + directive + '. Change it to "\'none\'" in your source list. Force this by enabling loose mode.')
+          if (SOURCELISTS_WITH_UNSAFES.indexOf(directive) !== -1) {
+            assertThrowsWithDirective(key, ['unsafe-inline'], '"unsafe-inline" must be quoted in ' + directive + '. Change it to "\'unsafe-inline\'" in your source list. Force this by enabling loose mode.')
+            assertThrowsWithDirective(key, ['unsafe-eval'], '"unsafe-eval" must be quoted in ' + directive + '. Change it to "\'unsafe-eval\'" in your source list. Force this by enabling loose mode.')
           }
-        } else if (directiveInfo.type === 'pluginTypes') {
-          it('errors with an empty array', function () {
-            throwTest(set([]), 'plugin-types must have at least one value. To block everything, set plugin-types to ["\'none\'"].')
-          })
+        })
 
-          it('errors when called with an array that contains non-strings', function () {
-            throwTest(set(['application/x-shockwave-flash', 420]), '"420" is not a valid plugin type. Only non-empty strings are allowed.')
-          })
-
-          it('errors with unquoted "none"', function () {
-            throwTest(set(['none']), '"none" must be quoted in plugin-types. Change it to "\'none\'" in your source list. Force this by enabling loose mode.')
-          })
-
-          it("errors when called with 'self'", function () {
-            throwTest(set(['self']), '"self" does not make sense in plugin-types. Remove it.')
-            throwTest(set(["'self'"]), '"\'self\'" does not make sense in plugin-types. Remove it.')
-          })
-
+        if (SOURCELISTS_WITH_UNSAFES.indexOf(directive) === -1) {
           it('errors when called with unsafe-inline or unsafe-eval', function () {
-            throwTest(set(['unsafe-inline']), '"unsafe-inline" does not make sense in ' + directiveKey + '. Remove it.')
-            throwTest(set(['unsafe-eval']), '"unsafe-eval" does not make sense in ' + directiveKey + '. Remove it.')
-            throwTest(set(["'unsafe-inline'"]), '"\'unsafe-inline\'" does not make sense in ' + directiveKey + '. Remove it.')
-            throwTest(set(["'unsafe-eval'"]), '"\'unsafe-eval\'" does not make sense in ' + directiveKey + '. Remove it.')
-          })
-
-          it('errors when called with non-array values', function () {
-            [
-              null,
-              undefined,
-              true,
-              {},
-              ''
-            ].forEach(function (value) {
-              throwTest(set(value), '"' + value + '" is not a valid value for plugin-types. Use an array of strings.')
-            })
-          })
-        } else if (directiveInfo.type === 'sandbox') {
-          it('errors with an empty array', function () {
-            throwTest(set([]), 'sandbox must have at least one value. To block everything, set sandbox to `true`.')
-          })
-
-          it('errors when called with non-array values', function () {
-            [
-              null,
-              undefined,
-              {},
-              ''
-            ].forEach(function (value) {
-              throwTest(set(value), '"' + value + '" is not a valid value for sandbox. Use an array of strings or `true`.')
-            })
-          })
-
-          it('errors when called with unsupported directives', function () {
-            throwTest(set(['allow-forms', undefined]), '"undefined" is not a valid sandbox directive. Remove it.')
-            throwTest(set(['allow-forms', 123]), '"123" is not a valid sandbox directive. Remove it.')
-            throwTest(set(['allow-foo']), '"allow-foo" is not a valid sandbox directive. Remove it.')
-            throwTest(set(['self']), '"self" is not a valid sandbox directive. Remove it.')
-            throwTest(set(["'self'"]), '"\'self\'" is not a valid sandbox directive. Remove it.')
-            throwTest(set(['none']), '"none" is not a valid sandbox directive. Remove it.')
-            throwTest(set(["'none'"]), '"\'none\'" is not a valid sandbox directive. Remove it.')
-          })
-        } else if (directiveInfo.type === 'reportUri') {
-          it('errors when called with non-string values', function () {
-            [
-              null,
-              undefined,
-              {},
-              { length: 0 },
-              { length: 2 },
-              [],
-              ['example.com'],
-              123,
-              true,
-              ''
-            ].forEach(function (value) {
-              throwTest(set(value), '"' + value + '" is not a valid value for ' + directiveKey + '. Use a non-empty string.')
-            })
-          })
-        } else if (directiveInfo.type === 'boolean') {
-          it('errors when called with non-boolean values', function () {
-            [
-              null,
-              undefined,
-              {},
-              [],
-              ['example.com'],
-              123,
-              '',
-              'true',
-              'false',
-              [true]
-            ].forEach(function (value) {
-              throwTest(set(value), '"' + value + '" is not a valid value for ' + directiveKey + '. Use `true` or `false`.')
-            })
-          })
-        } else {
-          it('should never run this!', function () {
-            assert(false, directiveInfo.type + ' directive type is untested!')
+            assertThrowsWithDirective(key, ['unsafe-inline'], '"unsafe-inline" does not make sense in ' + directive + '. Remove it.')
+            assertThrowsWithDirective(key, ['unsafe-eval'], '"unsafe-eval" does not make sense in ' + directive + '. Remove it.')
+            assertThrowsWithDirective(key, ["'unsafe-inline'"], '"\'unsafe-inline\'" does not make sense in ' + directive + '. Remove it.')
+            assertThrowsWithDirective(key, ["'unsafe-eval'"], '"\'unsafe-eval\'" does not make sense in ' + directive + '. Remove it.')
           })
         }
       })
     })
   })
+
+  BOOLEAN_DIRECTIVES.forEach(function (directive) {
+    [directive, camelize(directive)].forEach(function (key) {
+      describe(key + ' directive, a boolean', function () {
+        it('errors when called with non-boolean values', function () {
+          [
+            null,
+            undefined,
+            {},
+            [],
+            ['example.com'],
+            123,
+            '',
+            'true',
+            'false',
+            [true]
+          ].forEach(function (value) {
+            assertThrowsWithDirective(key, value, '"' + value + '" is not a valid value for ' + directive + '. Use `true` or `false`.')
+          })
+        })
+      })
+    })
+  })
+
+  PLUGINTYPE_DIRECTIVES.forEach(function (directive) {
+    [directive, camelize(directive)].forEach(function (key) {
+      describe(key + ' directive', function () {
+        it('errors with an empty array', function () {
+          assertThrowsWithDirective(key, [], directive + ' must have at least one value. To block everything, set ' + directive + ' to ["\'none\'"].')
+        })
+
+        it('errors with a non-array', function () {
+          [
+            null,
+            undefined,
+            true,
+            {},
+            '',
+            'https://example.com',
+            function () {}
+          ].forEach(function (value) {
+            assertThrowsWithDirective(key, value, '"' + value + '" is not a valid value for ' + directive + '. Use an array of strings.')
+          })
+        })
+
+        it('errors when called with an array that contains non-strings', function () {
+          assertThrowsWithDirective(key, ['application/x-shockwave-flash', 420], '"420" is not a valid plugin type. Only non-empty strings are allowed.')
+        })
+
+        it('errors with unquoted values', function () {
+          assertThrowsWithDirective(key, ['none'], '"none" must be quoted in ' + directive + '. Change it to "\'none\'" in your source list. Force this by enabling loose mode.')
+        })
+
+        it("errors when called with values that don't make sense, like 'self', 'unsafe-inline', and 'unsafe-eval'", function () {
+          assertThrowsWithDirective(key, ['self'], '"self" does not make sense in ' + directive + '. Remove it.')
+          assertThrowsWithDirective(key, ['unsafe-inline'], '"unsafe-inline" does not make sense in ' + directive + '. Remove it.')
+          assertThrowsWithDirective(key, ['unsafe-eval'], '"unsafe-eval" does not make sense in ' + directive + '. Remove it.')
+          assertThrowsWithDirective(key, ["'self'"], '"\'self\'" does not make sense in ' + directive + '. Remove it.')
+          assertThrowsWithDirective(key, ["'unsafe-inline'"], '"\'unsafe-inline\'" does not make sense in ' + directive + '. Remove it.')
+          assertThrowsWithDirective(key, ["'unsafe-eval'"], '"\'unsafe-eval\'" does not make sense in ' + directive + '. Remove it.')
+        })
+      })
+    })
+  })
+
+  URI_DIRECTIVES.forEach(function (directive) {
+    [directive, camelize(directive)].forEach(function (key) {
+      describe(key + ' directive, a URI', function () {
+        it('errors when called with non-string values', function () {
+          [
+            null,
+            undefined,
+            {},
+            { length: 0 },
+            { length: 2 },
+            [],
+            ['example.com'],
+            123,
+            true,
+            ''
+          ].forEach(function (value) {
+            assertThrowsWithDirective(key, value, '"' + value + '" is not a valid value for ' + directive + '. Use a non-empty string.')
+          })
+        })
+      })
+    })
+  })
+
+  describe('sandbox directive', function () {
+    assert.deepEqual(SANDBOX_DIRECTIVES, ['sandbox'])
+
+    it('errors with an empty array', function () {
+      assertThrowsWithDirective('sandbox', [], 'sandbox must have at least one value. To block everything, set sandbox to `true`.')
+    })
+
+    it('errors when called with non-array values', function () {
+      [
+        null,
+        undefined,
+        {},
+        '',
+        0,
+        1
+      ].forEach(function (value) {
+        assertThrowsWithDirective('sandbox', value, '"' + value + '" is not a valid value for sandbox. Use an array of strings or `true`.')
+      })
+    })
+
+    it('errors when called with unsupported directives', function () {
+      assertThrowsWithDirective('sandbox', ['allow-forms', undefined], '"undefined" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ['allow-forms', 123], '"123" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ['allow-foo'], '"allow-foo" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ['self'], '"self" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ["'self'"], '"\'self\'" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ['none'], '"none" is not a valid sandbox directive. Remove it.')
+      assertThrowsWithDirective('sandbox', ["'none'"], '"\'none\'" is not a valid sandbox directive. Remove it.')
+    })
+  })
 })
+
+function assertThrowsWithArg (arg, expectedMessage) {
+  assert.throws(
+    function () {
+      if (arg === 'NO_ARGUMENTS') {
+        csp()
+      } else {
+        csp(arg)
+      }
+    },
+    function (err) {
+      var isError = err instanceof Error
+      var isMessageOk = err.message === expectedMessage
+      return isError && isMessageOk
+    },
+    'Calling with ' + arg + ' did not throw'
+  )
+}
+
+function assertThrowsWithDirective (directiveKey, directiveValue, expectedMessage) {
+  var arg = { directives: {} }
+  arg.directives[directiveKey] = directiveValue
+  assertThrowsWithArg(arg, expectedMessage)
+}
